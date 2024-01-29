325253b3a3792ec8c53408ab261d35602ea9f00f
anjali <anjali.patel.30925@gmail.com>
2024-01-30 00:12:19 +0530

    adding files

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './header.css';
+import { IoLocation } from "react-icons/io5";
+import { FaCaretSquareDown } from "react-icons/fa";
 // import '../../style/commonclasses.css';
 
 const Header=()=>
@@ -7,6 +9,19 @@ const Header=()=>
     return(
         <div className="maxwidth header">
             <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="zomatoclonelogo" className="header-logo"/>
+            <div className="header_right">
+                <div className="header_location_search_container">
+                    <div className="loction_wrapper">
+                        <div className="location_icon_name">
+                            <div className="allcenter location_icon"><IoLocation/></div>
+                            <div>bangalore</div>
+                        </div>
+                        <div className="allcenter"><FaCaretSquareDown /></div>
+                           
+                    </div>
+                </div>
+            </div>
+        
         </div>
     );
 }
