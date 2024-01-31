4e9c6ee25cd5bffaa2a44eabff8f4db7017d86df
anjali <anjali.patel.30925@gmail.com>
2024-01-31 07:49:18 +0530

    adding files

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import './header.css';
 import { IoLocation } from "react-icons/io5";
 import { FaCaretSquareDown } from "react-icons/fa";
+import { BiSearchAlt } from "react-icons/bi";
+import { FaAngleDown } from "react-icons/fa";
+
+
 // import '../../style/commonclasses.css';
 
 const Header=()=>
@@ -14,14 +18,22 @@ const Header=()=>
                     <div className="loction_wrapper">
                         <div className="location_icon_name">
                             <div className="allcenter location_icon"><IoLocation/></div>
-                            <div>bangalore</div>
+                            <div>Bangalore</div>
                         </div>
                         <div className="allcenter"><FaCaretSquareDown /></div>
-                           
+                    </div>
+                    <div className="location_search_separator"></div>
+                    <div className="header_search_bar">
+                        <div className="allcenter search_icon" ><BiSearchAlt /></div>
+                        <input type="text"  placeholder="Search for a dish " className="search_box" />
                     </div>
                 </div>
+                <div className="profile_wrapper">
+                    <img src="https://www.drawhipo.com/wp-content/uploads/2022/07/Fast-Food-Color-3-Pizza-Curved.png" alt="profile" className="header_profile_img"/>
+                    <div>Anj</div>
+                    <div className="allcenter profile_option_icon"><FaAngleDown /></div>
+                </div>
             </div>
-        
         </div>
     );
 }
